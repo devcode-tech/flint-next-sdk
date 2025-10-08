49ae22f8c299bb160bc42bb301fa796d21c50f0d
devcode-tech <devshothive@gmail.com>
2025-10-08 17:54:38 +0530

    Fixed glitch issue

---
+++ public/dist/components/fields/RadioField.d.ts
@@ -0,0 +1,3 @@
+import React from "react";
+import { FormFieldProps } from "@/lib/types";
+export declare function RadioField({ field, value, onChange, error, className }: FormFieldProps): React.JSX.Element;
